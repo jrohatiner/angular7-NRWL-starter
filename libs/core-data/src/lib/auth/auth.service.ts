b53b9ab7fcdf1ae0609f6c4d1fbc703fd076bca0
jrohatiner <judithrohatiner@gmail.com>
2018-11-13 12:48:28 -0500

    new files and changes

---
+++ libs/core-data/src/lib/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//import { environment } from '@env/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+//import { AuthService } from './auth.service';
+import { Project } from '../projects/project.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  model = 'auth/login'
+  isAuthenticated$ = new BehaviorSubject(false);
+
+  constructor(private http: HttpClient) {
+    this.setToken(this.getToken());
+  }
+
+  getUrl() {
+    return `${environment.apiEndpoint}${this.model}`;
+  }
+
+  login(email, password) {
+    return this.http.post(this.getUrl(), { email, password});
+  }
+
+  logout() {
+    this.setToken('');
+  }
+
+  // TOKEN
+  setToken(token) {
+    localStorage.setItem('token', token);
+    this.isAuthenticated$.next(token !== ''); // Could be more Robust
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+}
